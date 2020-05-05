a61c2534838a4dbc563f2ae107baa4e463ea80c3
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-05-05 17:46:40 +0200

    main page finished

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -4,17 +4,17 @@ const Prices = () => {
     return (
         <div className={"price-container component"}>
             <div className={"contentContainer"}>
-            <div className={"price-box"}>
+            <div className={"price-box price1"}>
                 <h2>Tajin</h2>
                 <p> 32 cm séles</p>
-                <p> 22 - 28 magas </p>
+                <p> 22 - 28 cm magas </p>
                 <p> Lángállo mázzal</p>
                 <h3>23.000 Ft .-</h3>
             </div>
-            <div className={"price-box"}>
-                <h2>Egyedi tajin</h2>
+            <div className={"price-box price2"}>
+                <h2>Egyedi Tajin</h2>
                 <p> Egyedi méretben </p>
-                <p> Egyedi mintákkal</p>
+                <p> Egyedi mintával</p>
                 <p> Lángállo mázzal</p>
                 <h3>25.000 Ft-tól </h3>
             </div>
