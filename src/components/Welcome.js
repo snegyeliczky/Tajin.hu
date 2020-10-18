aa7f4748b6dfa49c20b30bdfa550a7e1798a2d91
100027033 <laszlo.jordan@ge.com>
2020-10-18 13:13:02 +0200

    use async await instead of promise chain, some minor text corrections, backgournd image update, added image to welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,33 +3,35 @@ import React, {useEffect, useState} from "react";
 
 
 const Welcome = () => {
-    const [text,setText] = useState("")
+    const [news,setText] = useState("");
+    const publicFolder = process.env.PUBLIC_URL;
 
-    let getTextFromFile = () => {
-        fetch("news/news.txt")
-            .then((r) => r.text())
-            .then(text => {
-                console.log(text);
-                if (text.length>2){
-                    setText(text);
-                }
-            })
-    };
+
+    const getNewsFromFile = async () => {
+        const r = await fetch("news/news.txt");
+        const text = await r.text();
+        if( text.length > 2 ) {
+            setText( text );
+        }
+    }
 
     useEffect(()=>{
-        getTextFromFile()
+        getNewsFromFile()
     },[]);
 
 
     return (
         <div id="Welcome" className={"component"}>
+            <div id={"welcomeImage"} className={"Welcome-image-container"}>
+                <img src={publicFolder + "/images/welcome_image.jpg"} alt="weclome image" className={"Welcome-image"}/>
+            </div>
             <div id={"aboutMe"} className={"Welcome-container"}>
                 <p> Jordán Ági vagyok, fazekas és négy gyermekes családanya.
-15 éve készítek sütő- és főzőedényeket, melyek között a tajine számomra a legkedvesebb. Talán, mert "gyökértelen fazekasnak" tartom magam, így nyitott voltam a hagyományos fazekasságtól eltérő formavilág felé.
+2005 óta készítek sütő- és főzőedényeket, melyek között a tajine számomra a legkedvesebb. Talán, mert "gyökértelen fazekasnak" tartom magam, így nyitott voltam a hagyományos fazekasságtól eltérő formavilág felé.
 Emellett a tajine edény nagy felületű kúpos teteje kíváló lehetőség az egyedi díszítésre.
                 </p>
-                { text.length>3
-                    ?<div className={"infoText"}><h3>- ! -  </h3>{text}</div>
+                { news.length>3
+                    ?<div className={"infoText"}><h3>- ! -  </h3>{news}</div>
                     :""
                 }
 
