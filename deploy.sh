2e0f8a42fa7d0b37f0b4e0d6de1faa66acf1c0f0
100027033 <laszlo.jordan@ge.com>
2020-12-13 06:46:49 +0100

    deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ $1 = prod ]
 then
 	export REACT_APP_ENV=production
 else
-	export REACT_APP_ENV=staging_$1
+	export REACT_APP_ENV=staging-$1
 fi
 rm -rf tajine.deploy
 echo $REACT_APP_ENV
@@ -13,6 +13,10 @@ npm run build
 mkdir tajine.deploy
 
 cp -r build tajine.deploy/
+if [ $1 != prod ]
+then 
+	cp app.staging.yaml app.$REACT_APP_ENV.yaml
+fi
 cp -r app.${REACT_APP_ENV}.yaml tajine.deploy/app.yaml
 cd tajine.deploy
 if [ $1 = prod ]
